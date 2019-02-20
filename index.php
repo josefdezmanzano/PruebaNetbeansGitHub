a263f0b018660a2fbb3e4a23e80a417969592e6f
seillo <seillo@seillo-GL552JX>
2019-02-20 11:04:51 +0100

    Revert "Esto es la Prueba perfecta"
    This reverts commit cc898c2d1bd95de5cadaceac0aa1dafcb0bd179a.

--- index.php
+++ index.php
@@ -15,18 +15,10 @@ and open the template in the editor.
         
         
         
-   echo "holi";
-   
+        
         ?>
         <p>
-   
             
-            madreeeeeeeeeeeeeeeeeeeeee
-            
-            e
-            e
-            e
-            e
         -----
             No se esto es un ejemplo para ver el cambio
         -----
